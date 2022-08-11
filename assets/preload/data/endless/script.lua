f324ac653ee9f7a00d222e9cb8abaee9aa7a115e
CoelhoTheDourado <110208982+CoelhoTheDourado@users.noreply.github.com>
2022-08-11 09:45:27 -0300

    Add files via upload

---
+++ assets/preload/data/endless/script.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bf');
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx');
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'Fun Is Infinite');
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd');
+end
