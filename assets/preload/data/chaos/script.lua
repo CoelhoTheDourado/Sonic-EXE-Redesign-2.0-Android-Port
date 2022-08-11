2b23c906efea2d1620d20ff836529e76b0b246c6
CoelhoTheDourado <110208982+CoelhoTheDourado@users.noreply.github.com>
2022-08-11 09:39:39 -0300

    Add files via upload

---
+++ assets/preload/data/chaos/script.lua
@@ -0,0 +1,99 @@
+
+local xx = 520;
+local yy = 450;
+local xx2 = 820;
+local yy2 = 450;
+local ofs = 60;
+local followchars = true;
+local del = 0;
+local del2 = 0;
+
+
+function onUpdate()
+	if del > 0 then
+		del = del - 1
+	end
+	if del2 > 0 then
+		del2 = del2 - 1
+	end
+    if followchars == true then
+        if mustHitSection == false then
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+        else
+
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+	    if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','')
+    end
+    
+end
+function onUpdate(elapsed)
+
+  if curStep >= 0 then
+
+    songPos = getSongPosition()
+
+    local currentBeat = (songPos/1000)*(bpm/50)
+
+    doTweenY(dadTweenY, 'dad', 300-30*math.sin((currentBeat*0.25)*math.pi),0.001)
+
+  end
+
+end
+function opponentNoteHit()
+    health = getProperty('health')
+    if getProperty('health') > 0.1 then
+        setProperty('health', health- 0.0125);
+    end
+end
+
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bfpixel');
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx-pixel');
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'Fleetway Game Over');
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'Continue');
+end
